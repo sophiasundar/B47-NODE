2f66bcd159cdc74b9f702187f5bf3b863639a06d
sophiasundar <sophiagifta17@gmail.com>
2024-01-03 11:58:53 +0530

    commit

--- HallBooking/index.js
+++ HallBooking/index.js
@@ -8,75 +8,75 @@ const PORT = process.env.PORT;
 
 const allHall = [
     {
-        "room-name": "Party-hall",
-        "room-id": 101,
-        "seats-available": 100,
+        "roomName": "Party-hall",
+        "roomId": 101,
+        "seatsAvailable": 100,
         "amenities":"air-conditioning, fans, lights",
-        "price for 1-hour":2000,
+        "priceFor1Hour":2000,
         "bookingDetails":[
             {
                 "customerName": "charlie puth",
                 "date": "29-11-2023",
-                "start-time": "12:00 PM",
-                "end-time": "2:00 PM",
-                "booked-status": "confirmed",
-                "booking-Id": "08",
-                "booked-Date": "29-05-2023"
+                "startTime": "12:00 PM",
+                "endTime": "2:00 PM",
+                "bookedStatus": "confirmed",
+                "bookingId": "08",
+                "bookingDate": "29-05-2023"
             },
             {
                 "customerName": "syvia",
                 "date": "1-11-2023",
-                "start-time": "2:00 PM",
-                "end-time": "5:00 PM",
-                "booked-status": "Waiting",
-                "booking-Id": "15",
-                "booked-Date": "17-09-2023"
+                "startTime": "2:00 PM",
+                "endTime": "5:00 PM",
+                "bookedStatus": "Waiting",
+                "bookingId": "15",
+                "bookingDate": "17-09-2023"
             },
             {
                 "customerName": "olivia",
                 "date": "31-12-2023",
-                "start-time": "11:00 PM",
-                "end-time": "1:00 AM",
-                "booked-status": "confirmed",
-                "booking-Id": "28",
-                "booked-Date": "14-06-2023"
+                "startTime": "11:00 PM",
+                "endTime": "1:00 AM",
+                "bookedStatus": "confirmed",
+                "bookingId": "28",
+                "bookingDate": "14-06-2023"
             }
 
         ]
     },
     {
-        "room-name": "banquet-hall",
-        "room-id": 108,
-        "seats-available": 1500,
+        "roomName": "banquet-hall",
+        "roomId": 108,
+        "seatsAvailable": 1500,
         "amenities":"air-conditioning, audio-visual effects, catering facilities",
-        "price for 1-hour":5000,
+        "priceFor1Hour":5000,
         "bookingDetails":[
             {
                 "customerName": "john eliot",
                 "date": "1-1-2024",
-                "start-time": "12:00 PM",
-                "end-time": "2:00 PM",
-                "booked-status": "confirmed",
-                "booking-Id": "29",
-                "booked-Date": "12-10-2023"
+                "startTime": "12:00 PM",
+                "endTime": "2:00 PM",
+                "bookedStatus": "confirmed",
+                "bookingId": "29",
+                "bookingDate": "12-10-2023"
             },
             {
                 "customerName": "salim ali",
                 "date": "17-2-2024",
-                "start-time": "2:00 PM",
-                "end-time": "5:00 PM",
-                "booked-status": "confirmed",
-                "booking-Id": "100",
-                "booked-Date": "17-12-2023"
+                "startTime": "2:00 PM",
+                "endTime": "5:00 PM",
+                "bookedStatus": "confirmed",
+                "bookingId": "100",
+                "bookingDate": "17-12-2023"
             },
             {
                 "customerName": "indira",
                 "date": "31-2-2024",
-                "start-time": "11:00 PM",
-                "end-time": "1:00 AM",
-                "booked-status": "confirmed",
-                "booking-Id": "203",
-                "booked-Date": "26-10-2023"
+                "startTime": "11:00 PM",
+                "endTime": "1:00 AM",
+                "bookedStatus": "confirmed",
+                "bookingId": "203",
+                "bookingDate": "26-10-2023"
             }
 
         ]
@@ -102,21 +102,38 @@ app.use(express.json());
         res.send('welcome to party hall👋🎉🎊🥳')
         })
 
+        // app.post('/allhall', async(req, res)=> {  
+        //     const addHall = req.body;
+        //     console.log(addHall)
+        //     const result = await client
+        //     .db("node")
+        //     .collection("hall-api")
+        //     .insertMany(addHall);
+        //     res.send(result);
+            
+        // });
+
+
 
         // 1. Creating a Room with: # Number of seats available, 
             // # Amenities in Room # Price for 1 Hour
             
-            // add room
+            // create room
+               
              app.post('/allhall', async(req, res)=> {  
-                const addHall = req.body;
-                console.log(addHall)
-                const result = await client
-                .db("node")
-                .collection("hall-api")
-                .insertMany(addHall);
-                res.send(result);
-                
-            });
+            const addHall = req.body;
+            console.log(addHall)
+            const result = await client
+            .db("node")
+            .collection("hall-api")
+            .insertMany(addHall);
+            res.send(result);
+            
+        });
+
+
+
+
 
         // 2. Booking a Room with: # Customer Name, 
             // # Date, # Start Time, # End Time, # Room Id
