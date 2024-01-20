fa1c43233ee63a862a2f493e3b84b0e92011f54a
sophiasundar <sophiagifta17@gmail.com>
2024-01-20 12:37:41 +0530

    commit

--- assign-student/index.js
+++ assign-student/index.js
@@ -4,11 +4,15 @@ import * as dotenv from "dotenv";
 import { studentsRouter } from "./routes/routeStudents.js";
 import { mentorsRouter } from "./routes/routeMentors.js";
 
+
 const app = express();
-const PORT = 8000;
+// const PORT = 8000;
+console.log(process.env);
+const PORT = process.env.PORT;
 dotenv.config();
 
-const MONGO_URL = "mongodb://0.0.0.0:27017";
+// const MONGO_URL = "mongodb://0.0.0.0:27017";
+const MONGO_URL = process.env.MONGO_URL;
 
 async function createConnection(){
     const client = new MongoClient(MONGO_URL);
@@ -25,11 +29,17 @@ app.use(express.json());
   app.get('/welcome',(req,res)=>{
     res.send('welcome to the mentor-students portal 🧑‍🏫 🏫 🧑‍🎓')
   })
+    
+  // students collection
   app.use("/students",studentsRouter);
-
+    
+  // mentors collection
   app.use("/mentors",mentorsRouter);
+    
+  // assign students to the mentor collection
+  // app.use("/stumen",stumenRouter);
 
   app.listen(PORT, ()=> 
 console.log("Server started on the PORT", PORT)
-)
+);
 
